01b136441db971dfb8670eb30720fd232a392cba
Jia Huang <iamfat@gmail.com>
2017-10-25 08:52:15 +0800

    use an empty ucloud.config.js

---
+++ ucloud.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    publicKey: '',
+    privateKey: '',
+    apiBase: 'https://api.ucloud.cn',
+    gapperBase: 'http://gapper.in',
+    allowedUsers: {
+        // '@project': ['user1', 'user2']
+        // 'host': ['user3']
+        // '*': ['user4']
+    }
+}
